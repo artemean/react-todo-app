9ebc7b52f50c5b7e03fca6697b4cd6c49f0170e7
Andrii Artemenko <andrey.artemenko@gmail.com>
2018-07-22 21:59:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    enteredValue: "",
+    todoList: []
+  };
+  addTodo(value) {
+    this.setState({
+      todoList: [
+        ...this.state.todoList,
+        { id: this.state.todoList.length, value, isDone: false }
+      ]
+    });
+  }
+  handleKeyDown = event => {
+    if (event.key === "Enter") {
+      this.addTodo(event.target.value);
+      this.inp.value = "";
+    }
+  };
+  removeTodo = id => () => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.filter(el => el.id !== id)
+    }));
+  };
+  toggleDone = id => () => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(el => {
+        return el.id === id ? { ...el, isDone: !el.isDone } : el;
+      })
+    }));
+  };
+  render() {
+    console.log(this.state.todoList);
+    return (
+      <div className="App">
+        <input
+          placeholder="What needs to be done?"
+          ref={inp => {
+            this.inp = inp;
+          }}
+          onKeyDown={this.handleKeyDown}
+        />
+        {this.state.todoList.length > 0 ? (
+          <TodoList
+            items={this.state.todoList}
+            removeTodo={this.removeTodo}
+            toggleDone={this.toggleDone}
+          />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+function TodoList({ items, removeTodo, toggleDone }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <li>
+          <input
+            type="checkbox"
+            checked={item.isDone}
+            onChange={toggleDone(item.id)}
+          />
+          <span className={item.isDone ? "done-item" : ""}>{item.value}</span>
+          <span onClick={removeTodo(item.id)} className="remove-icon" />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
